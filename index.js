58b426066759bdc1cf8c8479a14379ff60538bf5
ziyu <ziyu@caibaopay.com>
2018-03-20 10:13:04 +0800

    完成组件抽离

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import Form from 'lib/components/Form';
-import InputRow from 'lib/components/InputRow';
-import SelectRow from 'lib/components/SelectRow';
-
-module.exports = {
+import Form from './lib/components/Form';
+import InputRow from './lib/components/InputRow';
+import SelectRow from './lib/components/SelectRow';
+export {
     Form,
     InputRow,
     SelectRow,
